d75d1ff78bf1e2ed41144e4be81056fa1ba39284
Arsalan <jaffri.arsalan786@gmail.com>
2025-06-03 01:05:18 +0500

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,20 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+    build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          reacticons: ['react-icons'],
+          bootstrap: ['react-bootstrap'],
+          
+        }
+      },
+      chunkSizeWarningLimit: 1000, // Increase limit if needed
+    }
+  },
+  base:'/',
   css: {
     preprocessorOptions: {
       scss: {
