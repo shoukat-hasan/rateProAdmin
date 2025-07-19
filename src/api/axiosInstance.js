e86c39ad0446e5d3fe83ebbdafe49d3dbdd85ad1
Muhammad Shoukat Hasan <shoukat.hasan@gmail.com>
2025-07-19 23:35:15 +0500

    Update AxiosInstance.js

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://rateprobackend-production.up.railway.app/api',
-  // baseURL: import.meta.env.VITE_API_BASE_URL,
+  // baseURL: 'https://rateprobackend-production.up.railway.app/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
