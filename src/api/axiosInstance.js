6cf78bf8bf698f881715c3d5106ddd279110aabc
Muhammad Shoukat Hasan <shoukat.hasan@gmail.com>
2025-10-03 09:22:10 +0500

    remove refresh interceptor

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -311,7 +311,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
